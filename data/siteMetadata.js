15a0c7918c3ebd7e9c1ccc01fb58e80b7c423758
Anomitro Paul <anomitropaul@Anomitros-Mac-Studio.local>
2025-02-13 13:28:22 -0500

    updated metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A blog about my experiences and thoughts',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.anomitropaul.com',
+  siteRepo: 'https://github.com/paulano1/personal-website',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
